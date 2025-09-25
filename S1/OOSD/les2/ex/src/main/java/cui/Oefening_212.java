ff13a08f56c21954bea19339ac6cf6fe721a5262
Dohn Joe <tim.beliachenko@gmail.com>
2025-09-25 14:18:31 +0200

    OOSD: ex2 oef12 corrected

--- S1/OOSD/les2/ex/src/main/java/cui/Oefening_212.java
+++ S1/OOSD/les2/ex/src/main/java/cui/Oefening_212.java
@@ -1,12 +1,29 @@
 package cui;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class Oefening_212 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Geef een postcode(4 cijfers): ");
-        int postcode = sc.nextInt();
-
+        String postcode = sc.nextLine();
+        if (postcode.length() != 4) {
+            System.out.println("Postcode is niet geldig");
+            return;
+        }
+        try(Scanner fsc = new Scanner (new File("src/additional_files/postcodes.csv"))){
+            while(fsc.hasNext()){
+                String line =  fsc.nextLine();
+                String[] data = line.split(",");
+                if (data[0].equals(postcode)){
+                    System.out.printf("Postnummer %s komt overeen met de stad %s", data[0], data[1]);
+                    return;
+                }
+            }
+            System.out.printf("Postnummer %s staat niet op de lijst",  postcode);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
